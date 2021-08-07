70ca441a6115d746a09c8df9ce635fc9ae11ca18
Johan-Mi <johanmi@protonmail.com>
2021-08-07 09:09:51 +0200

    Make `Show LispAST` instance pretty-print
    This patch replaces the derived `Show` instance for `LispAST` with a
    custom one that prints the syntax trees as Lisp expressions just like
    the ones that they get parsed from (other than some slight differences
    in whitespace and number formatting). This should make debugging of ASTs
    much easier.

--- src/LispAST.hs
+++ src/LispAST.hs
@@ -3,6 +3,7 @@ module LispAST
   ) where
 
 import qualified Data.Text as T
+import Text.Printf (printf)
 
 data LispAST
   = LispNum Double
@@ -10,4 +11,11 @@ data LispAST
   | LispSym T.Text
   | LispNode LispAST [LispAST]
   | LispUnquote LispAST
-  deriving (Eq, Show)
+  deriving (Eq)
+
+instance Show LispAST where
+  show (LispNum n) = show n
+  show (LispString s) = printf "\"%s\"" s
+  show (LispSym s) = T.unpack s
+  show (LispNode fun args) = printf "(%s)" $ unwords $ show <$> (fun : args)
+  show (LispUnquote ast) = ',' : show ast
