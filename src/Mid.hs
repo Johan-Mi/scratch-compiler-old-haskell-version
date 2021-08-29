b34c6eb1da3ba1cb3d4fa3958049ae2a90adac99
Johan-Mi <johanmi@protonmail.com>
2021-08-29 14:53:38 +0200

    Rename 'scene' to 'stage'

--- src/Mid.hs
+++ src/Mid.hs
@@ -1,7 +1,7 @@
 module Mid
   ( Program
   , mkProgram
-  , scene
+  , stage
   , sprites
   ) where
 
@@ -13,13 +13,13 @@ import Mid.Sprite (Sprite, isStage, mkSprite)
 
 data Program =
   Program
-    { _scene :: Sprite
+    { _stage :: Sprite
     , _sprites :: [Sprite]
     }
   deriving (Show)
 
-scene :: Lens' Program Sprite
-scene f prg = (\sc -> prg {_scene = sc}) <$> f (_scene prg)
+stage :: Lens' Program Sprite
+stage f prg = (\sc -> prg {_stage = sc}) <$> f (_stage prg)
 
 sprites :: Lens' Program [Sprite]
 sprites f prg = (\spr -> prg {_sprites = spr}) <$> f (_sprites prg)
@@ -28,8 +28,8 @@ mkProgram :: [LispAST] -> Either MidError Program
 mkProgram asts = do
   spritesAndStages <- traverse mkSprite asts
   let (stages, sprites') = partition isStage spritesAndStages
-  stage <-
+  stage' <-
     case stages of
       [theStage] -> Right theStage
       xs -> Left $ WrongStageCount $ length xs
-  return $ Program stage sprites'
+  return $ Program stage' sprites'
