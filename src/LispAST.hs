94ee915b402065793a449d2f1734cc1c84928510
Johan-Mi <johanmi@protonmail.com>
2021-09-27 20:06:44 +0200

    Rewrite the macro system
    This cleans up the code a bit and should also allow macro definitions to
    use previously defined macros.

--- src/LispAST.hs
+++ src/LispAST.hs
@@ -3,11 +3,13 @@ module LispAST
   , isTheFunction
   , asTheFunction
   , getSym
+  , subTrees
   ) where
 
 import Control.Monad (guard)
 import Data.Functor (($>))
 import qualified Data.Text as T
+import Lens.Micro (Traversal')
 import Text.Printf (printf)
 
 data LispAST
@@ -37,3 +39,10 @@ asTheFunction _ _ _ = Nothing
 getSym :: LispAST -> Maybe T.Text
 getSym (LispSym sym) = Just sym
 getSym _ = Nothing
+
+subTrees :: Traversal' LispAST LispAST
+subTrees _ ast@(LispNum _) = pure ast
+subTrees _ ast@(LispString _) = pure ast
+subTrees _ ast@(LispSym _) = pure ast
+subTrees f (LispNode x xs) = LispNode <$> f x <*> traverse f xs
+subTrees f (LispUnquote x) = LispUnquote <$> f x
--- src/Macro.hs
+++ src/Macro.hs
@@ -1,29 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Macro
   ( expandMacros
   ) where
 
--- TODO: Support nested macros
-import Data.List (partition)
+import Control.Monad (foldM)
+import Data.Function (on)
+import Data.Functor ((<&>))
+import Data.Maybe (fromMaybe)
+import Data.Monoid (First(..))
 import qualified Data.Text as T
-import LispAST (LispAST(..), getSym, isTheFunction)
+import LispAST (LispAST(..), asTheFunction, getSym, subTrees)
 import Text.Printf (printf)
 import Utils.Either (maybeToRight)
 
-type Macro = LispAST -> Either MacroError LispAST
-
 data MacroError
-  = FunctionMacroUsedAsSymbol T.Text
-  | WrongArgCount T.Text Int Int
+  = WrongArgCount T.Text Int Int
   | NonSymbolParam T.Text
+  | NonSymbolMacroName
   | UnknownMetaVar T.Text T.Text
-  | UnquoteNonSymbol T.Text
+  | InvalidMacroDefinition -- Very unspecific, TODO: Better errors
   deriving (Eq)
 
 instance Show MacroError where
-  show (FunctionMacroUsedAsSymbol name) =
-    printf "`%s` is a function macro but was used as a symbol" name
   show (WrongArgCount name expected got) =
     printf
       "function macro `%s` expected %d arguments but got %d"
@@ -31,59 +31,61 @@ instance Show MacroError where
       expected
       got
   show (NonSymbolParam name) =
-    printf "non-symbol in parameter list of function macro %s" name
+    printf "non-symbol in parameter list of function macro `%s`" name
+  show NonSymbolMacroName = "non-symbol used as macro name"
   show (UnknownMetaVar name var) =
-    printf "unknown meta-variable `%s` in body of function macro %s" name var
-  show (UnquoteNonSymbol name) =
-    printf "tried to unquote non-symbol in function macro %s" name
+    printf "unknown meta-variable `%s` in body of function macro `%s`" name var
+  show InvalidMacroDefinition = "invalid macro definition"
 
-isMacro :: LispAST -> Bool
-isMacro = isTheFunction "macro"
+type Macro = LispAST -> Maybe (Either MacroError LispAST)
+
+mkMacro :: LispAST -> Maybe (Either MacroError Macro)
+mkMacro =
+  flip asTheFunction "macro" $ \case
+    [LispSym name, body] ->
+      Right $ \case
+        LispSym name'
+          | name' == name -> Just $ Right body
+        _ -> Nothing
+    [LispNode name params, body] -> do
+      name' <- maybeToRight NonSymbolMacroName $ getSym name
+      params' <- maybeToRight (NonSymbolParam name') $ traverse getSym params
+      return $ mkFuncMacro name' params' body
+    _ -> Left InvalidMacroDefinition
 
 mkFuncMacro :: T.Text -> [T.Text] -> LispAST -> Macro
-mkFuncMacro name params body = f
+mkFuncMacro name params body = f `asTheFunction` name
   where
-    f (LispSym _) = Left $ FunctionMacroUsedAsSymbol name
-    f (LispNode _ args)
+    f args
       | nargs /= nparams = Left $ WrongArgCount name nparams nargs
       | otherwise = subst name (zip params args) body
       where
         nargs = length args
         nparams = length params
-    f _ = undefined -- This should never happen
-
-mkMacro :: LispAST -> Either MacroError (T.Text, Macro)
-mkMacro (LispNode _ [LispSym name, body]) = Right (name, const $ Right body)
-mkMacro (LispNode _ [LispNode (LispSym name) astParams, body])
-  | Just params <- traverse getSym astParams =
-    Right (name, mkFuncMacro name params body)
-  | otherwise = Left $ NonSymbolParam name
-mkMacro _ = undefined -- This should never happen
 
 subst :: T.Text -> [(T.Text, LispAST)] -> LispAST -> Either MacroError LispAST
-subst name mvars (LispUnquote (LispSym sym)) =
-  maybeToRight (UnknownMetaVar name sym) $ sym `lookup` mvars
-subst name mvars (LispNode fun args) =
-  LispNode <$> subst name mvars fun <*> traverse (subst name mvars) args
-subst _ _ ast = Right ast
+subst name mvars = go
+  where
+    go (LispUnquote (LispSym sym)) =
+      maybeToRight (UnknownMetaVar name sym) $ lookup sym mvars
+    go ast = subTrees go ast
 
-withMacros :: [(T.Text, Macro)] -> LispAST -> Either MacroError LispAST
-withMacros _ (LispUnquote _) = error "TODO" -- How should this work?
-withMacros ms sym@(LispSym str) = maybe (Right sym) ($ sym) $ str `lookup` ms
-withMacros ms (LispNode fun args) = do
-  fun' <-
-    case withMacros ms fun of
-      Left (FunctionMacroUsedAsSymbol _) -> Right fun
-      f -> f
-  args' <- traverse (withMacros ms) args
-  let node' = LispNode fun' args'
-  case fun' of
-    LispSym str -> maybe (Right node') ($ node') $ str `lookup` ms
-    _ -> Right node'
-withMacros _ ast = Right ast
+expand :: Macro -> LispAST -> Either MacroError LispAST
+expand m = go
+  where
+    go ast = subTrees go ast >>= \ast' -> fromMaybe (pure ast') $ m ast'
 
 expandMacros :: [LispAST] -> Either MacroError [LispAST]
-expandMacros ast = do
-  let (macroDefs, exprs) = partition isMacro ast
-  macros <- traverse mkMacro macroDefs
-  traverse (withMacros macros) exprs
+expandMacros =
+  fmap snd .
+  foldM
+    (\(macros, accum) ast -> do
+       ast' <- expand macros ast
+       case mkMacro ast' of
+         Just m ->
+           m <&> \m' -> (getFirst . ((<>) `on` (First .)) macros m', accum)
+         Nothing -> return (macros, accum ++ [ast']))
+    (getFirst . foldMap (First .) builtinMacros, [])
+
+builtinMacros :: [Macro]
+builtinMacros = []
